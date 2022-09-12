feeb00a7ff715f7464fcde340bb2521e6dc32427
Zhassulan Kaitanov <kaitanov.work@gmail.com>
2022-09-12 16:43:04 +0600

    Init
    Add project structure

--- src/main/java/kata/academy/eureka/EurekaApp.java
+++ src/main/java/kata/academy/eureka/EurekaApp.java
@@ -1,4 +1,4 @@
-package kata.academy.eurekaserver;
+package kata.academy.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServer {
+public class EurekaApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServer.class, args);
+        SpringApplication.run(EurekaApp.class, args);
     }
 }
